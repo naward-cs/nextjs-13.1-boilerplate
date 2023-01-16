4b3fb6fe659724a71c23fcc85523855bfd3e905b
naward-cs <nicholas.ward.cyber@gmail.com>
2023-01-16 11:53:22 -0800

    added next-seo

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+const currentGitBranchName = require('current-git-branch')
+
 const nextConfig = {
+  images: {
+    minimumCacheTTL: 60,
+    disableStaticImages: true,
+  },
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
   reactStrictMode: true,
+  env: {
+    GIT_BRANCH: currentGitBranchName(),
+  },
 }
 
 module.exports = nextConfig
